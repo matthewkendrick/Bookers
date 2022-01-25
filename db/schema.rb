ec4d86e62f9f79f4214f9662cc1aa647320d43e2
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-01-25 22:15:06 +0000

    [Update]3-11

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,8 @@ ActiveRecord::Schema.define(version: 2022_01_25_043505) do
   create_table "books", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.string "body"
   end
 
 end

3703bee41c4e269dfc82e9b9ebb791c711fa764c
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-01-28 04:15:51 +0000

    [Finish]3-11

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,52 +1,62 @@
 class BooksController < ApplicationController
+  # 投稿一覧の表示
   def index
     @books = Book.all
-    @new_book = Book.new
+    @book = Book.new
   end
 
-  def new
-  end
-
+  # 新規投稿
   def create
     @books = Book.all
     @book = Book.new(book_params)
-  end
 
-   if @new_book.save
-     flash[:notice] = "The book was successfully created."
-     redirect_to book_path(@book)
-   else
-     render "index"
+    # バリデーションチェック
+    if @book.save
+      flash[:notice] = "The book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :index
     end
   end
 
+  # 詳細画面の表示
   def show
     @book = Book.find(params[:id])
   end
 
+  # 編集画面の表示
   def edit
     @book = Book.find(params[:id])
   end
 
+  # 編集の反映
   def update
     @book = Book.find(params[:id])
 
-   if @book.update(book_params)
-     flash[:notice] = "The book was successfully updated."
-     redirect_to book_path(@book)
-   else
-     render "edit"
-   end
-  end
-
-  def destroy
-    book = Book.find(params[:id])
-    if book.destroy
-      flash[:notice] = "The book was successfully deleted."
-      redirect_to books_path
+    # バリデーションチェック
+    if @book.update(book_params)
+      flash[:notice] = "The book was successfully updated."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :edit
     end
   end
 
+  # 投稿削除
+  def destroy
+    @book = Book.find(params[:id])
+
+    # バリデーションチェック
+    if @book.destroy
+      flash[:notice] = "The book was successfully deleted."
+      redirect_to books_path
+    else
+      render :index
+    end
+  end
+
+  # ストロングパラメータ
   private
   def book_params
     params.require(:book).permit(:title, :body)
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_25_043505) do
+ActiveRecord::Schema.define(version: 2022_01_28_021055) do
 
   create_table "books", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.string "title"
-    t.string "body"
+    t.text "body"
   end
 
 end
